eb092cfe3cf42bfc9efb6c99f28403cd455eeddb
yousifKashef <39941169+yousifKashef@users.noreply.github.com>
2025-01-30 11:13:22 +0400

    Add files via upload

--- tests/integration_tests/test_graph.py
+++ tests/integration_tests/test_graph.py
@@ -1,11 +1,15 @@
 import pytest
 from langsmith import unit
 
-from agent import graph
+from react_agent import graph
 
 
 @pytest.mark.asyncio
 @unit
-async def test_agent_simple_passthrough() -> None:
-    res = await graph.ainvoke({"changeme": "some_val"})
-    assert res is not None
+async def test_react_agent_simple_passthrough() -> None:
+    res = await graph.ainvoke(
+        {"messages": [("user", "Who is the founder of LangChain?")]},
+        {"configurable": {"system_prompt": "You are a helpful AI assistant."}},
+    )
+
+    assert "harrison" in str(res["messages"][-1].content).lower()
--- tests/unit_tests/test_configuration.py
+++ tests/unit_tests/test_configuration.py
@@ -1,4 +1,4 @@
-from agent.configuration import Configuration
+from react_agent.configuration import Configuration
 
 
 def test_configuration_empty() -> None:
